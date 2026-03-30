d866b5c516615844460360b3e1d5265552624b23
Brady McLaughlin <78229080+bradyjmcl@users.noreply.github.com>
2026-03-30 10:26:29 -0400

    Update kaliUP.sh
    Modified GPG key for Sublime Text

--- kaliUP.sh
+++ kaliUP.sh
@@ -214,9 +214,9 @@ printf "\n ${GREEN}-_-_-_-_- Finished installing kerbrute -_-_-_-_- ${NC}\n\n"
 
 # Install sublime text
 printf "\n ${PURPLE}-_-_-_-_- Installing Sublime Text 4 -_-_-_-_- ${NC}\n\n"
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
+apt-get update
 apt-get install sublime-text
 printf "\n ${GREEN}-_-_-_-_- Finished installing Sublime Text 4 -_-_-_-_- ${NC}\n\n"
 
